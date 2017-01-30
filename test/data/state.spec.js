ac7b55f74cd4f1fb6575ab6dc71c252c3cff3193
Stefan Rimaila <stefan@rimaila.fi>
2017-01-30 02:06:11 +0100

    Prep work for incoming data handling tests

---
+++ test/data/state.spec.js
@@ -0,0 +1,24 @@
+import * as L from 'partial.lenses';
+import * as R from 'ramda';
+import { expect } from 'chai';
+
+import stateData from './state.json';
+
+describe('Game State', () => {
+  describe('Ships', () => {
+    const ids = [138, 685, 33, 53];
+    const ships = L.get('ships', stateData);
+
+    it('should get ships with an array of ids (basic partial lens getter)', () => {
+      const fleets = L.get('fleets', stateData);
+      const result = L.get(L.filter(x => ids.includes(x.id)), ships);
+
+      expect(result.length).to.equal(ids.length);
+    });
+
+    it('should get ships with an array of ids (curried partial lens)', () => {
+      const fleets = L.get('fleets', stateData);
+      const result = L.get(L.filter(R.any(R.prop('id'))));
+    });
+  });
+});
--- test/modules/handler-game-state.spec.js
+++
@@ -1,7 +0,0 @@
-import { expect } from 'chai';
-
-describe('Game state handler', () => {
-  it('should herp a derp', () => {
-    expect(true).to.equal(true);
-  });
-});
