1aab71c7262104819d51f0e4c3990d2e12933a45
Stefan Rimaila <stefan@rimaila.fi>
2017-02-09 00:09:27 +0100

    Add component index to app-ui

---
+++ app/modules/app-ui/components/index.js
@@ -0,0 +1,8 @@
+import MainView from './main-view';
+import Sidebar from './sidebar';
+import StatusBar from './status-bar';
+import TitleBar from './title-bar';
+import TopMenu from './top-menu';
+import * as Meta from './meta';
+
+export { MainView, Sidebar, StatusBar, TitleBar, TopMenu, Meta };
