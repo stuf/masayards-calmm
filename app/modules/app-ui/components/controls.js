70b08a8358037752abb678531dbbb0c8fb3de153
Stefan Rimaila <stefan@rimaila.fi>
2017-02-09 20:48:22 +0100

    Cleanup

--- app/modules/app-ui/components/controls.js
+++ app/modules/app-ui/components/controls.js
@@ -10,19 +10,28 @@ import * as U from 'karet.util';
 import * as R from 'ramda';
 import Kefir from 'kefir';
 
-const tick = (t: number) => Kefir.interval(t).toProperty(R.identity);
+type Fn1 = (a: any) => any;
+
+const isTestEnv = process.env.NODE_ENV === 'test';
+const id = R.identity;
+
+const tick = (t: number, f: Fn1 = R.identity) => Kefir.interval(t).toProperty(f);
 const timeDelta = (t: number): number => t - +(new Date());
 
-export const KeyValueLabel = ({ key, value, ...props }: *) =>
-  <div {...props}>{key}: {value}</div>;
-
-export const Duration = ({ until, interval = 1000, prefix, suffix, ...props }: *) =>
+const DurationControl = ({ until, prefix, suffix, interval = 1000, ...props }: *) =>
   <div {...props}>
     {prefix}
-    {process.env.NODE_ENV !== 'test' ? tick(interval).map(() =>
+    {tick(interval).map(() =>
       U.seq(timeDelta(until),
         U.clamp(0, Infinity),
         U.divide(U.__, 1000),
-        U.floor)) : 0} seconds left
+        U.floor))}
     {suffix}
   </div>;
+
+const DurationDummy = () => <div />;
+
+export const Duration = !isTestEnv ? DurationControl : DurationDummy;
+
+export const KeyValueLabel = ({ key, value, ...props }: *) =>
+  <div {...props}>{key}: {value}</div>;
